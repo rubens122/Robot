02bc0d80e5636f5fa8cbf36493f16603daea1303
rubens122 <149151111+rubens122@users.noreply.github.com>
2023-11-21 13:38:50 +0100

    Orientacion correcto

---
+++ src/main/java/org/iesalandalus/programacion/robot/modelo/Orientacion.java
@@ -0,0 +1,23 @@
+package org.iesalandalus.programacion.robot.modelo;
+
+public enum Orientacion {
+    NORTE("Norte"),
+    NORESTE("Noreste"),
+    ESTE("Este"),
+    SURESTE("Sureste"),
+    SUR("Sur"),
+    SUROESTE("Suroeste"),
+    OESTE("Oeste"),
+    NOROESTE("Noroeste");
+
+    private String nombre;
+
+    private Orientacion(String nombre){
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
